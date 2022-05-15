e6378aa6e708706a5b4174f62011447dc13ee77c
mahmoudmorsy201 <mahmoudahmedmorsy@gmail.com>
2022-05-15 09:29:52 +0200

    add initial project setup

---
+++ Mercury-Store/Mercury-Store/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mercury-Store
+//
+//  Created by mac hub on 15/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
