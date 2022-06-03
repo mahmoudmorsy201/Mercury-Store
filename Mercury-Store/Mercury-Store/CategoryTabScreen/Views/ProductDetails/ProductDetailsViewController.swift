10d92c6d566a2089cde753de4578877a086fa3a1
mahmoudmorsy201 <mahmoudahmedmorsy@gmail.com>
2022-06-03 21:21:39 +0200

    fix logic issue in productDetailsViewModel and add subject for the images to push it to the cell

--- Mercury-Store/Mercury-Store/CategoryTabScreen/Views/ProductDetails/ProductDetailsViewController.swift
+++ Mercury-Store/Mercury-Store/CategoryTabScreen/Views/ProductDetails/ProductDetailsViewController.swift
@@ -15,30 +15,22 @@ class ProductDetailsViewController: UIViewController, UIScrollViewDelegate{
     @IBOutlet weak var productPriceLabel: UILabel!
     @IBOutlet weak var imageControl: UIPageControl!
     @IBOutlet weak var addToCart: UIButton!
-    @IBOutlet weak var productDetailsCollectionView: UICollectionView!
-    {
-        didSet {
-            productDetailsCollectionView.register(UINib(nibName: ProductDetailsImageCollectionCell.reuseIdentifier(), bundle: nil), forCellWithReuseIdentifier: ProductDetailsImageCollectionCell.reuseIdentifier())
-        }
-    }
-    var viewModel: ProductsDetailViewModelType? {
-        didSet {
-            self.configure()
-        }
-    }
-    @IBOutlet weak var favoriteBtn: UIButton!
-
-    //private var viewModel: ProductsDetailViewModelType?
-     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-       updateUi()
-       collectionViewFrame.onNext(self.productDetailsCollectionView.frame)
-
-        
+    @IBOutlet weak var productImagesCollectionView: UICollectionView! {
+        didSet {
+            productImagesCollectionView.register(UINib(nibName: ProductDetailsImageCollectionCell.reuseIdentifier(), bundle: nil), forCellWithReuseIdentifier: ProductDetailsImageCollectionCell.reuseIdentifier())
+        }
     }
+    
+    @IBOutlet weak var favoriteBtn: UIButton!
+    
+    private var viewModel: ProductsDetailViewModelType?
+    private let disposeBag = DisposeBag()
+    
+    private let collectionViewFrame = ReplaySubject<CGRect>.create(bufferSize: 1)
+    
+
+    
     init(with viewModel: ProductsDetailViewModelType) {
         super.init(nibName: nil, bundle: nil)
         self.viewModel = viewModel
@@ -47,32 +39,32 @@ class ProductDetailsViewController: UIViewController, UIScrollViewDelegate{
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    private func updateUi(){
-       
-        productTitleLabel.text = viewModel?.product.title
-        productPriceLabel.text = (viewModel?.product.variants[0].price ?? "") + " USD"
-        productDescription.text = viewModel?.product.bodyHTML
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUi()
+        collectionViewFrame.onNext(self.productImagesCollectionView.frame)
     }
-    
-    private let disposeBag = DisposeBag()
-    
-    private let collectionViewFrame = ReplaySubject<CGRect>.create(bufferSize: 1)
-    
-   
-    
+    private func updateUi(){
+        productTitleLabel.text = viewModel?.product.title
+        productPriceLabel.text = (viewModel?.product.variants[0].price ?? "") + " EGP"
+        productDescription.text = viewModel?.product.bodyHTML
+        self.configure()
+    }
 }
 
 extension ProductDetailsViewController {
     private func bindCollectionView() {
-        productDetailsCollectionView.dataSource = nil
-        productDetailsCollectionView.delegate = nil
-        productDetailsCollectionView.rx.setDelegate(self).disposed(by: disposeBag)
+        productImagesCollectionView.dataSource = nil
+        productImagesCollectionView.delegate = nil
+        productImagesCollectionView.rx.setDelegate(self).disposed(by: disposeBag)
         
         viewModel?.bannerObservable
-            .drive(productDetailsCollectionView.rx.items(cellIdentifier:  ProductDetailsImageCollectionCell.reuseIdentifier(), cellType:  ProductDetailsImageCollectionCell.self)) {indexPath, item, cell in
+            .drive(productImagesCollectionView.rx.items(cellIdentifier:  ProductDetailsImageCollectionCell.reuseIdentifier(), cellType:  ProductDetailsImageCollectionCell.self)) {indexPath, item, cell in
                 cell.item = item
             }
             .disposed(by: disposeBag)
+        viewModel?.sendImagesToCollection()
     }
     
     private func bindPageController() {
@@ -83,18 +75,18 @@ extension ProductDetailsViewController {
     
     private func bindCollectionViewToPageControll() {
         currentPage(
-            offset: productDetailsCollectionView.rx.contentOffset
+            offset: productImagesCollectionView.rx.contentOffset
                 .asObservable(),
             frame: collectionViewFrame.asObserver()
         )
         .bind(to: imageControl.rx.currentPage)
         .disposed(by: disposeBag)
     }
-        
+    
     private func currentPage(offset: Observable<CGPoint>, frame: Observable<CGRect>) -> Observable<Int> {
         return Observable.combineLatest(offset,frame)
             .map{ Int($0.0.x) / Int($0.1.width)}
-            
+        
     }
 }
 
@@ -114,7 +106,7 @@ extension ProductDetailsViewController : UICollectionViewDelegateFlowLayout {
         return 0.0
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width:productDetailsCollectionView.frame.width, height: productDetailsCollectionView.frame.height)
+        return CGSize(width:productImagesCollectionView.frame.width, height: productImagesCollectionView.frame.height)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
