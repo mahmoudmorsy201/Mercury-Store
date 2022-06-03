3e4021ac66494eb4a933b88c88a373f6aa7c5d90
raiaan <raiaanm@gmail.com>
2022-06-03 08:11:13 +0200

    refactoring category Items to use Rx instead of closures

--- Mercury-Store/Mercury-Store/CategoryTabScreen/Views/CategoryMain/CustomCells/CategoryItem.swift
+++ Mercury-Store/Mercury-Store/CategoryTabScreen/Views/CategoryMain/CustomCells/CategoryItem.swift
@@ -12,9 +12,6 @@ class CategoryItem: UICollectionViewCell {
     @IBOutlet weak var containerViewForCategoriesCollectionViewCell: UIView!
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var image: UIImageView!
-    var itemID:Int?
-    var selectType:String?
-    var cellClickAction:( (_ item:Int ,_ selectedType:String)->() )?
     override func awakeFromNib() {
         super.awakeFromNib()
         setupCell()
@@ -28,17 +25,6 @@ class CategoryItem: UICollectionViewCell {
     public func config(name:String , itemId:Int){
         title.text = name
         image.image = UIImage(named: name)
-        self.itemID = itemId
-        self.selectType = name
-        let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
-        containerViewForCategoriesCollectionViewCell.addGestureRecognizer(tap)
-    }
-    @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
-        guard let cellClickAction = cellClickAction else {
-            return
-        }
-        cellClickAction(itemID!, selectType!)
-        
     }
     private func setupCell() {
         containerViewForCategoriesCollectionViewCell.applyShadow(cornerRadius: 12)
