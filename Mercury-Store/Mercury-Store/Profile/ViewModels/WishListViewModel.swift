8edf0a237de9acf58703e41920419e95dc013bc0
raiaan <raiaanm@gmail.com>
2022-06-04 21:38:45 +0200

    favorite coreData --- base modeling -- update , restore  with view model

---
+++ Mercury-Store/Mercury-Store/Profile/ViewModels/WishListViewModel.swift
@@ -0,0 +1,61 @@
+//
+//  WishListViewModel.swift
+//  Mercury-Store
+//
+//  Created by Rain Moustfa on 04/06/2022.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+protocol WishListViewModelType {
+    var products: Driver<[SavedProductItem]> { get}
+    var error: Driver<String?> { get }
+    func getFavouriteItems()
+    func updateItem(item:SavedProductItem)
+    func deleteItem(item:SavedProductItem)
+}
+class WishListViewModel:WishListViewModelType{
+    var products: Driver<[SavedProductItem]>
+    var error: Driver<String?>
+    
+    private let productSubject = BehaviorRelay<[SavedProductItem]>(value: [])
+    private let errorSubject = BehaviorRelay<String?>(value: nil)
+    let coreDataModel:CoreDataModel
+    let disposeBag = DisposeBag()
+    init(){
+        coreDataModel = CoreDataModel.coreDataInstatnce
+        products = productSubject.asDriver(onErrorJustReturn: [])
+        error = errorSubject.asDriver(onErrorJustReturn: "Somthing went wrong")
+    }
+    func getFavouriteItems() {
+        self.productSubject.accept([])
+        self.errorSubject.accept(nil)
+        let items = self.coreDataModel.getItems(productState: productStates.favourite)
+        if(items.1 == nil){
+            self.productSubject.accept(items.0)
+            self.errorSubject.accept(nil)
+        }else {
+            self.errorSubject.accept(items.1?.localizedDescription)
+        }
+    }
+    
+    func updateItem(item: SavedProductItem) {
+        let state = self.coreDataModel.update(updateitem: item)
+        if (state.1) {
+            getFavouriteItems()
+            self.errorSubject.accept(nil)
+        }
+        else {
+            self.errorSubject.accept("Somthing went wrong")
+            
+        }
+    }
+    
+    func deleteItem(item: SavedProductItem) {
+        
+    }
+    
+    
+    
+}
---
+++ Mercury-Store/Mercury-Store/Support/CoreData/Models/SavedProductItem.swift
@@ -0,0 +1,27 @@
+//
+//  SavedProductItem.swift
+//  Mercury-Store
+//
+//  Created by Rain Moustfa on 03/06/2022.
+//
+
+import Foundation
+struct SavedProductItem{
+    var productID:Decimal
+    var productTitle:String
+    var productImage:String
+    var productPrice:Double
+    var productQTY:Int
+    var producrState:Int
+}
+extension SavedProductItem {
+    init() {
+        productTitle = ""
+        productID = 0
+        productImage = ""
+        productPrice = 0
+        productQTY = 0
+        // 0 favourite -- 1 cart -- 2cart
+        producrState = 0
+    }
+}
