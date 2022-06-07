23f448eb6045f086d266bf49327b13d9c5422fbc
mahmoudmorsy201 <mahmoudahmedmorsy@gmail.com>
2022-06-07 20:50:59 +0200

    Add switch button and bind it to array to sort it alphabetically if true or reversed for false value

--- Mercury-Store/Mercury-Store/Home/Views/Search/Views/SearchViewController.swift
+++ Mercury-Store/Mercury-Store/Home/Views/Search/Views/SearchViewController.swift
@@ -19,18 +19,17 @@ class SearchViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var productSearchbar: UISearchBar!
     @IBOutlet weak var sliderPrice: UISlider!
     @IBOutlet weak var filterBtn: UIButton!
+    @IBOutlet weak var switchSort: UISwitch!
     
-    @IBOutlet weak var sortBtn: UIButton!
     @IBOutlet weak var productListCollectionView: UICollectionView! {
         didSet {
             productListCollectionView.register(UINib(nibName: BrandProductsCollectionViewCell.reuseIdentifier(), bundle: nil), forCellWithReuseIdentifier: BrandProductsCollectionViewCell.reuseIdentifier())
         }
     }
 
-
-    
     var viewModel: ProductSearchViewModel?
     private var bag = DisposeBag()
+    
     var filterIsPressed = true
     var errorView: UIView? {
         return nil
@@ -48,13 +47,13 @@ class SearchViewController: UIViewController, UIScrollViewDelegate {
         super.viewDidLoad()
         hideSlider()
         bindToSearchValue()
-        bindCollectionView()
         bindSelectedItem()
         bindFilterBtn()
         bindSlider()
         bindPrice()
         bindActivity()
         viewModel?.fetchData()
+        bindSortBtn()
         bind()
     }
     
@@ -63,13 +62,11 @@ class SearchViewController: UIViewController, UIScrollViewDelegate {
             fatalError("Couldn't unwrap viewModel")
         }
         
-        let inputData = viewModel.valueChanged
         productListCollectionView.delegate = nil
         productListCollectionView.dataSource = nil
         productListCollectionView.rx.setDelegate(self).disposed(by: bag)
         
-        let output =
-        viewModel.bind(SearchInput(viewLoaded: inputData, filterTapped: rx.filter, sortTapped: rx.sort))
+        let output = viewModel.bind()
         
         output.filteredItems.bind(to: productListCollectionView.rx.items(dataSource: dataSource()))
             .disposed(by: bag)
@@ -93,36 +90,37 @@ class SearchViewController: UIViewController, UIScrollViewDelegate {
             .bind(to: viewModel!.value)
             .disposed(by: bag)
     }
+    private func bindSortBtn() {
+        switchSort.rx.value
+            .bind(to: viewModel!.sortAlphabetically)
+            .disposed(by: bag)
+    }
     private func bindPrice(){
-        // If you want to listen and bind to a label
         viewModel?.value.asDriver()
             .map { "EGP \($0) " }
             .drive(maximumPrice.rx.text)
             .disposed(by: bag)
     }
+    
     private func bindFilterBtn(){
         filterBtn.rx.tap.bind {
             self.filterBtnIsPressed()
             
         }.disposed(by: bag)
     }
-    private func bindCollectionView() {
-//        viewModel.filteredProductList.bind(to: productListCollectionView.rx.items(cellIdentifier: BrandProductsCollectionViewCell.reuseIdentifier(), cellType: BrandProductsCollectionViewCell.self)) { index, item , cell in
-//            cell.item = item
-//        }.disposed(by: bag)
-    }
+
     private func bindActivity() {
-//        viewModel.isLoadingData.drive(ProgressHUD.rx.isAnimating)
-//        .disposed(by: bag)
+        viewModel?.isLoadingData.drive(ProgressHUD.rx.isAnimating)
+        .disposed(by: bag)
     }
+    
     private func bindSelectedItem() {
-//        productListCollectionView.rx.modelSelected(Product.self).subscribe{ [weak self] item in
-//
-//            self?.viewModel.goToProductDetailFromSearch(with: item)
-//        }.disposed(by: bag)
-        
-        
+        productListCollectionView.rx.modelSelected(Product.self).subscribe{ [weak self] item in
+
+            self?.viewModel?.goToProductDetailFromSearch(with: item)
+        }.disposed(by: bag)
     }
+    
     private func filterBtnIsPressed(){
         if filterIsPressed{
             filterIsPressed = false
@@ -149,23 +147,6 @@ extension SearchViewController {
             return cell
         }
     }
-}
-
-
-extension Reactive where Base: SearchViewController {
-    var filter: Observable<Void> {
-        base.filterBtn.rx.tap
-            .map{_ in }
-    }
-    
-    var sort: Observable<Void> {
-        base.sortBtn.rx.tap
-            .map{ _ in }
-    }
-    
-    
-    
-    
 }
 
 
