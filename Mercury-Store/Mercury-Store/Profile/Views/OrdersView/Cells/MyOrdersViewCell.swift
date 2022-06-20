6e5880b4b7ac51a75b8f0413160f72035d05429d
raiaan <raiaanm@gmail.com>
2022-06-20 13:39:30 +0200

    printing address in orders list

--- Mercury-Store/Mercury-Store/Profile/Views/OrdersView/Cells/MyOrdersViewCell.swift
+++ Mercury-Store/Mercury-Store/Profile/Views/OrdersView/Cells/MyOrdersViewCell.swift
@@ -24,11 +24,10 @@ class MyOrdersViewCell: UITableViewCell {
         viewContainer.bounds = CGRect(x: 8, y: 8, width: self.frame.width-8 , height: self.frame.height-8 )
         createdAt.text = "\(order.name) \(order.createdAt.formatted(.dateTime))"
         price.text = "Order total price: \(order.totalPrice) \(order.currency)"
+        paid.text = "address: \(order.shippingAddress?.city  ?? "") - \(order.shippingAddress?.province ?? "") "
     }
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
 }
