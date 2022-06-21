b2fecc30bdca910d4c17960f393fe6958ee4f7bf
mahmoudmorsy201 <mahmoudahmedmorsy@gmail.com>
2022-06-21 17:19:05 +0200

    added validation on createNewAddress form

--- Mercury-Store/Mercury-Store/Profile/Views/AddressView/CreateAddressDetailsViewController.swift
+++ Mercury-Store/Mercury-Store/Profile/Views/AddressView/CreateAddressDetailsViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import Toast_Swift
 import RxSwift
 import RxCocoa
+import DropDown
 
 class CreateAddressDetailsViewController: UIViewController {
     
@@ -17,13 +18,18 @@ class CreateAddressDetailsViewController: UIViewController {
     @IBOutlet weak var closeButton: UIButton!
     @IBOutlet weak var addAddressBtn: UIButton!
     @IBOutlet weak var countryTxt: UITextField!
-    @IBOutlet weak var cityTxt: UITextField!
     @IBOutlet weak var AddressTxt: UITextField!
     @IBOutlet weak var phoneTxt: UITextField!
+    @IBOutlet weak var errorLabelOnAddress: UILabel!
+    @IBOutlet weak var errorLabelOnPhone: UILabel!
+    @IBOutlet weak var errorLabelOnCity: UILabel!
+    @IBOutlet weak var selectCityButton: UIButton!
+    
     // MARK: - Properties
     //
     private var viewModel: AddressViewModelType!
     private let disposeBag = DisposeBag()
+    private let dropDown = DropDown()
     let connection = NetworkReachability.shared
     // MARK: - Set up
     //
@@ -42,7 +48,10 @@ class CreateAddressDetailsViewController: UIViewController {
         setupBindings()
         observeViewModelOnValid()
         bindCloseButton()
-        
+        observePhoneIsValid()
+        observeAddressIsValid()
+        selectedCity()
+        observeCityIsValid()
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -52,6 +61,9 @@ class CreateAddressDetailsViewController: UIViewController {
         self.addAddressBtn.tintColor = ColorsPalette.labelColors
         self.addAddressBtn.configuration?.background.backgroundColor = ColorsPalette.lightColor
         closeButton.tintColor = ColorsPalette.lightColor
+        countryTxt.isEnabled = false
+        dropDown.anchorView = selectCityButton
+        dropDown.dataSource = EgyptCitiesArray.EgyptCities
     }
     // MARK: - Private handlers
     //
@@ -62,19 +74,52 @@ class CreateAddressDetailsViewController: UIViewController {
             }.disposed(by: disposeBag)
     }
     func setupBindings() {
-        cityTxt.rx.text.bind(to: viewModel.cityObservable).disposed(by: disposeBag)
-        countryTxt.rx.text.bind(to: viewModel.countryObservable).disposed(by: disposeBag)
         AddressTxt.rx.text.bind(to: viewModel.addressObservable).disposed(by: disposeBag)
         phoneTxt.rx.text.bind(to: viewModel.phoneObservable).disposed(by: disposeBag)
     }
     
-    func  observeViewModelOnValid(){
+    func observeAddressIsValid() {
+        viewModel.isNotEmptyAddress.subscribe(onNext: {[weak self] isValid in
+            self?.errorLabelOnAddress.text = isValid ? "Valid Address" : "Please fill address"
+            self?.errorLabelOnAddress.textColor = isValid ? .green : .red
+        }).disposed(by: disposeBag)
+    }
+    
+    func observePhoneIsValid() {
+        viewModel.isNotValidPhone.subscribe(onNext: {[weak self] isValid in
+            self?.errorLabelOnPhone.text = isValid ? "Valid Phone" : "Please enter a valid phone"
+            self?.errorLabelOnPhone.textColor = isValid ? .green : .red
+        }).disposed(by: disposeBag)
+    }
+    
+    func observeCityIsValid() {
+        viewModel.isNotValidCity.subscribe(onNext: {[weak self] isValid in
+            self?.errorLabelOnCity.text = isValid ? "Valid city" : "Please select a city"
+            self?.errorLabelOnCity.textColor = isValid ? .green : .red
+        }).disposed(by: disposeBag)
+    }
+    
+    func observeViewModelOnValid(){
         viewModel.isValidForm.bind(to: addAddressBtn.rx.isEnabled).disposed(by: disposeBag)
+    }
+    
+    private func selectedCity() {
+        selectCityButton.rx.tap.subscribe(onNext: { [weak self] _ in
+            self?.dropDown.selectionAction = { (index: Int, item: String) in
+                self?.selectCityButton.titleLabel?.text = item
+                self?.viewModel.acceptTitle(item)
+            }
+            
+            self?.dropDown.width = self?.view.bounds.width
+            self?.dropDown.direction = .bottom
+            self?.dropDown.show()
+        }).disposed(by: disposeBag)
+
     }
     // MARK: - IBActions
     @IBAction func didPressedOnAddAddress(_ sender: Any) {
         let user = viewModel.getUserFromUserDefaults()
-        viewModel.postAddress(AddressRequestItem(address1: AddressTxt!.text!, address2: AddressTxt!.text!, city: cityTxt!.text!, company: "iti", firstName:user!.username, lastName: user!.username, phone: phoneTxt!.text!, province: cityTxt!.text!, country: countryTxt!.text!, zip: "G1R 4P5", name: "\(user!.username)", provinceCode: "Cairo", countryCode: "EG", countryName: "Egypt"))
+        viewModel.postAddress(AddressRequestItem(address1: AddressTxt!.text!, address2: AddressTxt!.text!, city: (selectCityButton.titleLabel?.text)!, company: "iti", firstName:user!.username, lastName: user!.username, phone: phoneTxt!.text!, province: (selectCityButton.titleLabel?.text)!, country: countryTxt!.text!, zip: "G1R 4P5", name: "\(user!.username)", provinceCode: "Cairo", countryCode: "EG", countryName: "Egypt"))
           self.view.makeToast("You Have Created address!", duration: 3.0, position: .bottom)
 
     }
