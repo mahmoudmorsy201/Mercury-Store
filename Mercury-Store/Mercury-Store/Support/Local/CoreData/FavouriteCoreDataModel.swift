ed9bdfc5cdb32f7fe4ee089a0fdb05b8182653ac
raiaan <raiaanm@gmail.com>
2022-06-25 22:14:54 +0200

    rewrite inserting and deleteing favourite from coredata to handle multi userIDs

--- Mercury-Store/Mercury-Store/Support/Local/CoreData/FavouriteCoreDataModel.swift
+++ Mercury-Store/Mercury-Store/Support/Local/CoreData/FavouriteCoreDataModel.swift
@@ -7,15 +7,28 @@
 
 import Foundation
 import CoreData
+
 extension CoreDataModel{
     func deleteFavouriteProduct(productID:Int)->Bool{
-        if(isBothProduct(id: productID)){
-            var Product = getItemByID(productID: productID)
-            Product.producrState = productStates.cart.rawValue
-            return (update(updateitem: Product)).1
-        }else{
-            return delete(itemID: productID)
+        if isProductExist(id: productID){
+            var state = 0
+            var item = getItemByID(productID: productID)
+            if item.producrState == productStates.both.rawValue {
+                item.producrState = productStates.cart.rawValue
+                state = 1
+            }
+            if !item.user_id.contains(getCurrentUserId()!) && item.user_id.count > 1{
+                item.user_id = item.user_id.filter{$0 != getCurrentUserId()!}
+                state = 2
+            }
+            if state == 0{
+                return delete(itemID: productID)
+            }
+            else{
+                return update(updateitem: item).1
+            }
         }
+        return false
     }
     func toggleFavourite(product:SavedProductItem)->Bool{
         let isFavorite = isProductFavourite(id: Int(truncating: NSDecimalNumber(decimal: product.productID) ))
@@ -26,9 +39,9 @@ extension CoreDataModel{
         }
         return !isFavorite
     }
+    
     func isProductFavourite (id :Int) -> Bool {
             var results: [NSManagedObject] = []
-            
             let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: self.entity)
         fetchRequest.predicate =  NSPredicate(format: "\(productCoredataAttr.id.rawValue) = %@ AND (\(productCoredataAttr.state.rawValue)  = %@ OR \(productCoredataAttr.state.rawValue) = %@)", argumentArray: [id as CVarArg, productStates.favourite.rawValue , productStates.both.rawValue])
             do {
@@ -39,15 +52,30 @@ extension CoreDataModel{
             }
         return !results.isEmpty
         }
+    
     func insertFavouriteProduct(product:SavedProductItem)->Bool{
-        if (isCartProduct(id: Int(truncating: NSDecimalNumber(decimal: product.productID)))) {
-            var product = getItemByID(productID: Int(truncating: NSDecimalNumber(decimal: product.productID)))
-            product.producrState = productStates.both.rawValue
-            return update(updateitem: product).1
+        if isProductExist(id: Int(truncating: NSDecimalNumber(decimal: product.productID))){
+            var item = getItemByID(productID: Int(truncating: NSDecimalNumber(decimal: product.productID)))
+            if item.producrState == productStates.cart.rawValue {
+                item.producrState = productStates.both.rawValue
+            }
+            if !item.user_id.contains(getCurrentUserId()!){
+                item.user_id.append(getCurrentUserId()!)
+            }
+            return insert(item: item).1
         }
-        else if(!isProductExist(id: Int(truncating: NSDecimalNumber(decimal: product.productID)))){
+        else{
             return insert(item: product).1
         }
-        return false
+    }
+    
+    func getCurrentUserId()->Int?{
+        let sharedInstance: UserDefaults = UserDefaults.standard
+        do{
+            let user:User = try sharedInstance.getObject(forKey: "user", castTo: User.self)
+            return user.id
+        }catch( _){
+            return nil
+        }
     }
 }
