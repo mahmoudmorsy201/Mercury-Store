c34ee5717629a57c29484c106b2c928c5ec1a1ad
raiaan <raiaanm@gmail.com>
2022-06-27 12:17:13 +0200

    fix toast to be only  when favourite added

--- Mercury-Store/Mercury-Store/Utils/UIImageView+Shadow_SDWebImage.swift
+++ Mercury-Store/Mercury-Store/Utils/UIImageView+Shadow_SDWebImage.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit.UIImageView
+import SDWebImage
 
 extension UIImageView {
     func applyshadowWithCorner(containerView : UIView, cornerRadius : CGFloat){
@@ -23,4 +24,23 @@ extension UIImageView {
         self.clipsToBounds = true
         self.layer.cornerRadius = cornerRadius
     }
+}
+extension UIImageView {
+    // Image download and cache using SDWebImage
+    func downloadImage(url: URL, placeholder:UIImage?,imageIndicator:SDWebImageActivityIndicator, completion:((UIImage?, Error?) -> Void)?) {
+        self.sd_imageIndicator = imageIndicator
+        self.sd_setImage(with: url, placeholderImage: placeholder, options: .retryFailed, context: nil, progress: nil) { (img, error, cacheType, url) in
+            if error != nil {
+                if placeholder == nil {
+                    self.image =  UIImage(named: "placeholder")
+                } else {
+                    self.image = placeholder
+                }
+            }
+            if completion != nil {
+                completion!(img, error)
+            }
+        }
+    }
+    
 }
